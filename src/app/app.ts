eb3d5287eb85d0a405a2d1fa3f9a88eaa95a1e6f
Dagson Gabriel <dagson.g.01@gmail.com>
2026-01-05 20:28:36 -0300

    feat: implementa interface para um todo list básico com integração a api

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,56 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from './todo.interface';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('todo-app');
+export class AppComponent implements OnInit {
+  todos: Todo[] = [];
+  newTodoTitle: string = '';
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.loadTodos();
+  }
+
+  loadTodos(): void {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+  }
+
+  addTodo(): void {
+    if (!this.newTodoTitle.trim()) return;
+
+    const newTodo: Todo = {
+      id: 0,
+      title: this.newTodoTitle,
+      isComplete: false
+    };
+
+    this.todoService.addTodo(newTodo).subscribe(todo => {
+      this.todos.push(todo);
+      this.newTodoTitle = '';
+    });
+  }
+
+  toggleTodo(todo: Todo): void {
+    todo.isComplete = !todo.isComplete;
+    this.todoService.updateTodo(todo).subscribe();
+  }
+
+  deleteTodo(id: number): void {
+    this.todoService.deleteTodo(id).subscribe(() => {
+      this.todos = this.todos.filter(t => t.id !== id);
+    });
+  }
 }
---
+++ src/app/todo.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Todo } from './todo.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  private apiUrl = 'http://localhost:5192/todoitems';
+
+  constructor(private http: HttpClient) { }
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.apiUrl);
+  }
+
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.apiUrl, todo);
+  }
+
+  updateTodo(todo: Todo): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${todo.id}`, todo);
+  }
+
+  deleteTodo(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
